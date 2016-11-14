426fcf3b4784789dbd264104eb0cc194783ab6bf
Oren Weingrod <oren@siberia.io>
2016-11-14 14:26:56 -0500

    styled components for game tracker component

---
+++ client/components/GameTracker/GameTracker.jsx
@@ -0,0 +1,23 @@
+import React, { PropTypes } from 'react';
+import style from './GameTracker.css';
+
+
+const GameTracker = ({ currentIndex, totalImages }) => (
+  <div className={style.gameTracker}>
+    <div className={style.gameTrackerBack}>
+      <button className={style.gameTrackerBackButton}></button>
+    </div>
+    <div className={style.gameTrackerIndex}>{currentIndex} / {totalImages}</div>
+    <div className={style.gameTrackerClose}>
+      <button className={style.gameTrackerCloseButton}></button>
+    </div>
+    <hr className={style.gameTrackerProgress} />
+  </div>
+);
+
+GameTracker.propTypes = {
+  currentIndex: PropTypes.number.isRequired,
+  totalImages: PropTypes.number.isRequired
+};
+
+export default GameTracker;
