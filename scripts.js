f2fd1c0d8524fa973c5106eea816fb932a3e2df0
Oren Weingrod <oren@siberia.io>
2016-11-23 12:16:48 -0500

    initial commit of bliss.js rewrite

--- scripts.js
+++ scripts.js
@@ -1,67 +1,60 @@
-function setManyAttributes(elem /*attributes here*/) {
-  for (var i = 1; i < arguments.length; i+=2) {
-        elem.setAttribute(arguments[i], arguments[i+1]);
-    }
-}
+var answer1, answer2, answer3;
 
-function buildDom() {
-  var gameContainer = document.createElement('div');
-  setManyAttributes(gameContainer,
-    'id', 'gameContainer');
-
-  document.getElementById('container').appendChild(gameContainer);
-
-  var gameImage = document.createElement('img');
-
-  setManyAttributes(gameImage,
-    'id', 'gameImage',
-    'src', 'http://placekitten.com/200/175');
-
-  gameContainer.appendChild(gameImage);
-
-  var publisherLogo = document.createElement('img');
-
-  setManyAttributes(publisherLogo, 
-    'id', 'publisherLogo',
-    'src', 'http://u1.mgcdn.com/mimg/merimg/b_143952896098.png?v=141016');
-
-  gameContainer.appendChild(publisherLogo);
-
-  var additionalText = document.createElement('span');
-
-  setManyAttributes(additionalText,
-    'id', 'additionalText');
-
-  additionalText.innerHTML = 'For you';
-
-  gameContainer.appendChild(additionalText);
-
-  var callToAction = document.createElement('div');
-
-  setManyAttributes(callToAction,
-    'id', 'callToAction');
-
-  callToAction.innerHTML = 'Find your perfect <br> Little Black Dress style';
-
-  gameContainer.appendChild(callToAction);
-
-  var ctaButton = document.createElement('button');
-
-  setManyAttributes(ctaButton,
-    'id', 'ctaButton');
-
-  ctaButton.innerHTML = 'Play the Game';
-
-  gameContainer.appendChild(ctaButton);
-
-  gameContainer.addEventListener('click', function() {
-    while (gameContainer.hasChildNodes()) {
-      gameContainer.removeChild(gameContainer.lastChild);
-    }
+var buildInitialDom = function() {
+  var mainContainer = $('#container');
+  var gameContainer = $.create('div', {
+    id: 'gameContainer'
+  });
+  var gameImage = $.create('img', {
+    id: 'gameImage',
+    src: 'http://placekitten.com/200/175'
+  });
+  var publisherLogo = $.create('img', {
+    id: 'publisherLogo',
+    src: 'http://u1.mgcdn.com/mimg/merimg/b_143952896098.png?v=141016'
+  });
+  var callToAction = $.create('div', {
+    id: 'callToAction',
+    contents: 'Find your perfect Little Black dress style'
+  });
+  var additionalText = $.create('p', {
+    id: 'additionalText',
+    contents: 'For you'
+  });
+  var ctaButton = $.create('button', {
+    id: 'ctaButton', 
+    contents: 'Play the game'
   });
 
+  mainContainer.append(gameContainer);
+  gameContainer.append(gameImage);
+  gameContainer.append(publisherLogo);
+  gameContainer.append(additionalText);
+  gameContainer.append(callToAction);
+  gameContainer.append(ctaButton);
 
+  ctaButton.addEventListener('click', function(event) {
+    buildSecondaryDom();
+  });
 }
 
-buildDom();
+var buildGameDom = function() {
+  var gameImage = $.create('img', {
+    id: 'gameImage',
+    src: 'http://placekitten.com/200/175'
+  });
+
+  
+}
+
+buildInitialDom();
+
+
+
+
+
+
+
+
+
 
